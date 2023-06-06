d19249f4421de64f59dce2affd3d63a394c96277
N1 <pathaknishant003@gmail.com>
2023-06-06 10:50:31 -0400

    asd

---
+++ report2.py
@@ -0,0 +1,21 @@
+{GITHUB}========>>
+
+GitHub is a web-based platform that provides hosting for version control repositories using Git, a distributed version control system. It offers a wide range of features for collaborative software development, including source code management, issue tracking, project management, code review, and more. GitHub is widely used by developers and organizations to host and collaborate on software projects.
+
+Here are some key features and concepts related to GitHub:
+
+Git Repositories: GitHub allows you to create and manage Git repositories for your projects. A repository contains the entire history of a project, including its files, commits, branches, and tags.
+
+Version Control: GitHub uses Git's version control system, which tracks changes to files in a repository over time. It allows multiple developers to work on the same project simultaneously, coordinating their changes and merging them together.
+
+Collaboration: GitHub provides features for collaborative development, such as pull requests, which allow developers to propose changes to a repository and initiate code review and discussion. It also supports issues and project boards to track and manage tasks, bugs, and feature requests.
+
+Branching and Merging: Git's branching and merging capabilities are integral to GitHub. Developers can create branches to work on new features or fixes without affecting the main codebase. Changes made in a branch can be merged back into the main branch (usually called "master" or "main") once they are reviewed and approved.
+
+Forking: GitHub allows users to create their own copy ("fork") of a repository. This enables independent development and experimentation. Users can make changes in their forked repository and propose those changes back to the original repository through pull requests.
+
+GitHub Pages: GitHub provides a hosting service called GitHub Pages, allowing users to create and host static websites directly from their repositories.
+
+Integration and Automation: GitHub integrates with various development tools and services. It supports continuous integration and deployment (CI/CD) workflows, allowing you to automatically build, test, and deploy your applications. GitHub also offers an extensive marketplace of third-party apps and integrations.
+
+GitHub is widely used by individual developers, open-source projects, and organizations for version control, collaboration, and project management. It provides a user-friendly interface, documentation, and community features that make it easy to contribute to open-source projects and collaborate with other developers.
